ed8b50a37dca160665424511c0bf662a72a3415c
Ashley Waldner <ashleywaldner22@gmail.com>
2020-07-29 20:54:31 -0500

    deploy

--- server.js
+++ server.js
@@ -35,6 +35,20 @@ app.use(privateRoutes);
 
 // Start the API server
 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'garden'
+});
+};
+
+connection.connect();
+module.exports = connection;
+
 db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
