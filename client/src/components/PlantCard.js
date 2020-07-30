ac2e7f7f56e5687db7e851aa30e9b9ecff8f0462
Jacob Zirbel <jacobzirbel@gmail.com>
2020-07-29 21:18:35 -0500

    Merge pull request #30 from plantpeople/garden-page
    Garden page

--- client/src/components/PlantCard.js
+++ client/src/components/PlantCard.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../index.css";
 
 const PlantCard = (props) => {
+  const { garden, search, saved, handleClick, days } = props;
+  const [waterDays, setWaterDays] = useState(days);
+
+  let buttonName;
+  if (garden) {
+    buttonName = "Remove from My Garden";
+  } else if (search) {
+    buttonName = "Add to Saved";
+  } else if (saved) {
+    buttonName = "Remove from Saved";
+  }
+
   return (
     <div>
       <div className="card">
@@ -15,9 +27,28 @@ const PlantCard = (props) => {
 
         <div className="content">
           <p>
-            <strong> Name: </strong>
+            <strong> Name: {props.name} </strong>
           </p>
-          <button onClick={() => props.addToGarden()}>Add to my garden</button>
+          <button onClick={handleClick}>{buttonName}</button>
+          <div>
+            <span>Water this</span>
+            <select
+              value={waterDays}
+              onChange={(event) => {
+                setWaterDays(event.target.value);
+                // TODO update plant in database
+              }}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+            </select>
+            <span>times per week</span>
+          </div>
         </div>
       </div>
     </div>
