5462c3020cecb176bf151aa8c9de2018870d18f7
kaygrog <kaylagrogan23@gmail.com>
2020-08-08 11:08:54 -0500

    Additional calendar updates

--- client/src/components/GardenPage.js
+++ client/src/components/GardenPage.js
@@ -2,17 +2,19 @@ import React, { useState, useLayoutEffect } from "react";
 import PlantCard from "./PlantCard";
 import Calendar from "./Calendar";
 import API from "../util/API";
-import "../index.css"
+import "../index.css";
 const GardenPage = (props) => {
   const [weatherLoaded, setWeatherLoaded] = useState(false);
   const [rainDays, setRainDays] = useState([]);
   let { plantsArray } = props;
   const getWeather = () => {
     console.log("getWeather called");
-    API.getWeather(53202).then((response) => {
-      setRainDays(response.data);
-      setWeatherLoaded(true);
-    }).catch(console.log);
+    API.getWeather(90210)
+      .then((response) => {
+        setRainDays(response.data);
+        setWeatherLoaded(true);
+      })
+      .catch(console.log);
   };
 
   useLayoutEffect(getWeather, []);
@@ -24,12 +26,11 @@ const GardenPage = (props) => {
       days={plant.waterDays}
       key={plant.name}
       name={plant.name}
-      
     />
   ));
 
   return (
-    <div  className="myGarden">
+    <div className="myGarden">
       <div>
         <div className="plants">{plantCards}</div>
         {/* Create a Calendar w/ water data from all plants in My Garden */}
