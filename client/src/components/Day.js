f068c04d5e42f0be57694ccf4132fb3970063343
aj mac <macdonalda47@gmail.com>
2020-08-10 19:58:38 -0500

    reorganize calendar cards to display plant name, whether to water in table; insert images to indicate yes or no for watering

--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -3,15 +3,19 @@ import React from "react";
 const Day = (props) => {
   // Create array to hold JSX partials for plants
   var plantItems = [];
+  var table = <table></table>
+  var tableRow 
 
   // Loop through array from props to create JSX partial for each plant
   props.plantWaterArray.forEach((plant) => {
     plantItems.push(
       // Make sure each partial has a unique key to satisfy React's reqs
-      <div key={plant.name}>
-        <p>Name: {plant.name}</p>
-        <p>Water today? {plant.shouldWaterArray[props.day] ? "yes" : "no"}</p>
-      </div>
+    
+      <tr key={plant.name} >
+        <td>{plant.name}</td>
+        <td>{plant.shouldWaterArray[props.day] ? <img className="water" src="https://s.pngkit.com/png/small/10-100968_raindrop-clipart-raindrop-clipart-transparent-background.png"/> : <img className="water" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Stop.svg/1200px-Stop.svg.png"/> }
+        </td>
+     </tr>
     );
   });
 
@@ -21,7 +25,8 @@ const Day = (props) => {
         <h1>{props.day}</h1>
         <div className="water-info">
           <p>{props.willRain ? "rained" : "didnt rain"}</p>
-          <div>{plantItems}</div>
+          <table>
+        {plantItems}</table>
         </div>
       </div>
     </div>
